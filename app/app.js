7b74f62aed902e3fbf516673bff036a6a7d3ba7a
Ginger Barham <gingermostly@gmail.com>
2019-02-11 21:21:50 -0800

    Add delete/update button functionality

--- app/app.js
+++ app/app.js
@@ -3,18 +3,18 @@ Init app
 interact with DOM
 interact with localstorage
 
- */
+*/
 
 $(document).ready(function(){
 
   var alphabet = {0:'a', 1:'b', 2:'c', 3:'d', 4:'e', 5:'f', 6:'g', 7:'h', 8:'i', 9:'j', 10:'k', 11:'l', 12:'m', 13:'n', 14:'o', 15:'p', 16:'q', 17:'r', 18:'s', 19:'t', 20:'u', 21:'v', 22:'w', 23:'x', 24:'y'
-, 25:'z'};
+  , 25:'z'};
   var letter = _.random(25); //alphabet array and letter variable used for getting random letters when generating a random, unique key for each new snippet
   var dataKeys = Object.keys(localStorage);
 
   if(dataKeys){
     dataKeys.forEach(key =>{
-      $('.container-data').append(`<textarea class="save-data-item" data-keyValue="${key}">${localStorage[key]}</textarea><button class="btn-delete">DELETE</button><button class="btn-update">UPDATE</button>`);
+      $('.container-data').append(`<div class='data-wrapper'><textarea class="save-data-item" data-keyValue="${key}">${localStorage[key]}</textarea><button class="btn-delete">DELETE</button><button class="btn-update">UPDATE</button></div>`);
     });
   };
 
@@ -23,34 +23,31 @@ $(document).ready(function(){
     return unique;
   }; //Generate a random key for storing the code snippet
 
-  $('.btn-save').on('click', function(e){
+  $('.btn-clear').click(function(){
+    $('.input-code').val('');
+  });
+
+  $('.btn-save').click(function(){
     var codeData = $('.input-code').val();
     var keyData = key();
 
     localStorage.setItem(keyData, codeData);
-    // TODO make this vars make sense across the app
-    $('.container-data').append(`<textarea class="save-data-item" data-keyValue="${keyData}">${codeData}</textarea><button class="btn-delete">DELETE</button><button class="btn-update">UPDATE</button>`);
+    $('.container-data').append(`<div class='data-wrapper'><textarea class="save-data-item" data-keyValue="${keyData}">${codeData}</textarea><button class="btn-delete">DELETE</button><button class="btn-update">UPDATE</button></div>`);
     $('.input-code').val('');
   });
 
-$('.btn-delete').on('click', function(){
-    var itemKey = this.previousElementSibling.dataset.keyvalue;
-    localStorage.removeItem(itemKey);
-      this.previousElementSibling.remove();
-      this.nextSibling.remove();
-      this.remove();
-});
-  // update db
-    // need to expand when  more than 1 item is added
-
-  // delete item
-  // $('.container-data').on('click', '.display-data-item', function(e){
-  //   var keyData = e.currentTarget.dataset.keyvalue;
-  //   localStorage.removeItem(keyData);
-  //   $('.container-data').text('');
-  // });
-  // // delete all?
-  $('.btn-clear').click(function(){
-    $('.input-code').val('');
+  $('.container-data').on('click', function(e){
+    if($(e.target).hasClass('btn-delete')){
+      var itemKey = e.target.closest('.data-wrapper').querySelector('.save-data-item').dataset.keyvalue;
+      localStorage.removeItem(itemKey);
+      e.target.closest('.data-wrapper').remove();
+    }
+  });
+  $('.container-data').on('click', function(e){
+    if($(e.target).hasClass('btn-update')){
+      var itemKey = e.target.closest('.data-wrapper').querySelector('.save-data-item').dataset.keyvalue;
+      var itemData = e.target.closest('.data-wrapper').querySelector('.save-data-item').value;
+      localStorage.setItem(itemKey, itemData)
+    }
   });
 });
